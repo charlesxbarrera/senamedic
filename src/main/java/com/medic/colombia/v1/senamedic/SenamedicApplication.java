2c938ca2aeb96af5c2d7e0093ac3dff56a0c7c74
destajador83-create <destajador83@gmail.com>
2026-06-07 21:30:43 -0500

    Proyecto Spring Boot inicial

---
+++ src/main/java/com/medic/colombia/v1/senamedic/SenamedicApplication.java
@@ -0,0 +1,13 @@
+package com.medic.colombia.v1.senamedic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenamedicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenamedicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medic/colombia/v1/senamedic/SenamedicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medic.colombia.v1.senamedic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenamedicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
