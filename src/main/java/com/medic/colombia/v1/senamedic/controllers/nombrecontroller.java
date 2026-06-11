cbf3ec558afd03ce9972a88f5819d4318e389875
destajador83-create <destajador83@gmail.com>
2026-06-11 00:58:20 -0500

    Se corrige configuración del backend y conexión a la base de datos

--- src/main/java/com/medic/colombia/v1/senamedic/controllers/nombrecontroller.java
+++ src/main/java/com/medic/colombia/v1/senamedic/controllers/nombrecontroller.java
@@ -1,4 +1,45 @@
 package com.medic.colombia.v1.senamedic.controllers;
 
-public class nombrescontroller {
+import ch.qos.logback.core.read.ListAppender;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController("/nombre")
+
+public class nombrecontroller {
+
+    private ArrayList<String>nombres = new ArrayList<>();
+
+
+    @PostMapping("/{nombre}")
+    public String crear(@PathVariable("nombre") String nombre){
+
+        boolean isAdded = false;
+
+        if (!nombre.isEmpty()) {
+            this.nombres.add(nombre);
+            isAdded = true;
+
+        }
+
+        return "Nombre agragado" : "Nombre no agregado";
+
+    }
+    @GetMapping()
+    public String listar(){
+        return  "";
+    }
+
+    @PutMapping()
+    public String editar(){
+        return "";
+
+    }
+
+    @DeleteMapping
+    public String eliminar(){
+        return "";
+    }
+
 }
